824ff44e951a1dc3e3ed7dbc038115ff5eec37ce
huyparody <m30d3n2010@gmail.com>
2024-09-25 18:53:55 +0700

    Remove import MapboxDirections

--- OSRMTextInstructions/OSRMTextInstructions.swift
+++ OSRMTextInstructions/OSRMTextInstructions.swift
@@ -1,5 +1,4 @@
 import Foundation
-import MapboxDirections
 
 // Will automatically read localized Instructions.plist
 let OSRMTextInstructionsStrings = NSDictionary(contentsOfFile: Bundle(for: OSRMInstructionFormatter.self).path(forResource: "Instructions", ofType: "plist")!)!
