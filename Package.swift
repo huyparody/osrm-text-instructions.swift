b61891cd554425ff517feb891aac17ff3395fb62
huyparody <m30d3n2010@gmail.com>
2024-10-23 16:27:49 +0700

    Revert version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/mapbox/mapbox-directions-swift", from: "2.14.0")
+        .package(url: "https://github.com/mapbox/mapbox-directions-swift", from: "2.5.0")
     ],
     targets: [
         .target(
